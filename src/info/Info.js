e130b9ffe32d29a2b4e9c0a225ead6b1917ea829
abdelhalim-abdelsalam <108537602+abdelhalim-abdelsalam@users.noreply.github.com>
2023-06-26 19:07:51 +0300

    add my personal induction

--- src/info/Info.js
+++ src/info/Info.js
@@ -22,8 +22,8 @@ of what each of the values mean.
  */
 
 export const info = {
-  firstName: "Halim",
-  lastName: "Smith",
+  firstName: "Abd El-Halim",
+  lastName: "",
   initials: "js", // the example uses first and last, but feel free to use three or more if you like.
   position: "a Front-end Developer",
   selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
